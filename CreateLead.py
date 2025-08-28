afcf959384fea3e0577349b6b45c0d24394b69eb
bp-vidgets <system_account@bkpravo.ru>
2025-08-28 17:01:50 +0300

    Update CreateLead.py

--- CreateLead.py
+++ CreateLead.py
@@ -2,31 +2,61 @@ import os
 from fastapi import FastAPI
 import requests
 import json
+from pydantic import BaseModel
 app = FastAPI()
 URLBITRIX = os.environ['URLBITRIX']
 EMOJI = os.environ['EMOJI']
 SOURCE_ID = os.environ['SOURCE_ID']
-ASSIGNED_BY_ID = os.environ['ASSIGNED_BY_ID']
-@app.post("/addlead/")
-def read_root(NAME:str,
-              PHONE: str,
-              WMID: int| None = None,COMMENT: str| None = None,
-              UTM_SOURCE: str| None = None,UTM_MEDIUM:str| None = None,UTM_CAMPAIGN:str| None = None,UTM_CONTENT:str| None = None,UTM_TERM:str| None = None
-              ):
+
+class Item(BaseModel):
+    fields: dict
+    NAME: str
+    PHONE: int
+    COMMENT: str | None = None
+    RECORD: str | None = None
+    UTM_SOURCE: str | None = None
+    UTM_MEDIUM: str | None = None
+    UTM_CAMPAIGN: str | None = None
+    UTM_TERM: str | None = None
+    UTM_CONTENT: str | None = None
+  
+@app.post("/addlead/crm.lead.add.json")
+def read_post_root(item: Item):
     lead_data = {'fields':{
-            'TITLE':str(EMOJI + NAME),
-            'NAME': NAME,
+            'TITLE':item.fields['TITLE'],
+            'NAME': item.fields['NAME'],
             "STATUS_ID": "NEW",
-            "ASSIGNED_BY_ID ":ASSIGNED_BY_ID,
             "SOURCE_ID": SOURCE_ID,
-            "PHONE": [{ "VALUE": PHONE,"VALUE_TYPE": "OTHER","TYPE_ID": "PHONE"}],
-            'UTM_SOURCE': UTM_SOURCE,
-            'UTM_MEDIUM':UTM_MEDIUM,
-            'UTM_CAMPAIGN':UTM_CAMPAIGN,
-            'UTM_CONTENT':UTM_CONTENT,
-            'UTM_TERM':UTM_TERM,
-            'COMMENTS':COMMENT
+            "UTM_SOURCE":item.fields['UTM_SOURCE'],
+            "UTM_MEDIUM":item.fields['UTM_MEDIUM'],
+            "UTM_CAMPAIGN":item.fields['UTM_CAMPAIGN'],
+            "UTM_TERM":item.fields['UTM_TERM'],
+            "UTM_CONTENT":item.fields['UTM_CONTENT'],
+            "PHONE": [{ "VALUE": item.fields['PHONE'][0]["VALUE"],"VALUE_TYPE": "OTHER","TYPE_ID": "PHONE"}],
+            'COMMENTS': item.fields['COMMENTS']
+        
         }}
+    response = requests.post(str(f'{URLBITRIX}/crm.lead.add.json'), json=lead_data)
+    print(response)
+    answ = json.loads(response.text)
+    return {"data": answ['result']}
+
+@app.get("/addlead/")
+def read_get_root(item: Item):
+    lead_data = {'fields':{
+            'TITLE':str(EMOJI + item.NAME),
+            'NAME': item.NAME,
+            "STATUS_ID": "NEW",
+            "SOURCE_ID": SOURCE_ID,
+            "UTM_SOURCE":item.UTM_SOURCE,
+            "UTM_MEDIUM":item.UTM_MEDIUM,
+            "UTM_CAMPAIGN":item.UTM_CAMPAIGN,
+            "UTM_TERM":item.UTM_TERM,
+            "UTM_CONTENT":item.UTM_CONTENT,
+            "PHONE": [{ "VALUE": item.PHONE,"VALUE_TYPE": "OTHER","TYPE_ID": "PHONE"}],
+            'COMMENTS': item.COMMENT
+        }}
+    print(f'send data ={lead_data}')
     response = requests.post(str(f'{URLBITRIX}/crm.lead.add.json'), json=lead_data)
     print(response)
     answ = json.loads(response.text)
