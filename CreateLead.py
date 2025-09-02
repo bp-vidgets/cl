581ffcfaaa7f59d997411014405bb6526f7148ba
bp-vidgets <system_account@bkpravo.ru>
2025-09-02 11:25:46 +0300

    Update CreateLead.py

--- CreateLead.py
+++ CreateLead.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 import requests
 import json
 from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
 app = FastAPI()
 URLBITRIX = os.environ['URLBITRIX']
 EMOJI = os.environ['EMOJI']
@@ -10,7 +11,48 @@ SOURCE_ID = os.environ['SOURCE_ID']
 
 class Item(BaseModel):
     fields: dict
-  
+
+class Fields(BaseModel):
+    TITLE: str
+    NAME: Optional[str] = None
+    COMMENTS: Optional[str] = None
+    OPENED: Optional[str] = None
+    PHONE: Optional[List[Phone]] = None
+    SOURCE_DESCRIPTION: Optional[str] = None
+    UTM_SOURCE: Optional[str] = None
+    UTM_CONTENT: Optional[int] = None
+    UTM_TERM: Optional[int] = None
+
+class Params(BaseModel):
+    REGISTER_SONET_EVENT: Optional[str] = None
+
+class RequestModel(BaseModel):
+    fields: Fields
+    params: Optional[Params] = None
+    
+@app.post("/testprocess/")
+async def process(request: RequestModel):
+    lead_data = {'fields':{
+            'TITLE':request['NAME'],
+            'NAME': request['NAME'],
+            "STATUS_ID": "NEW",
+            "SOURCE_ID": SOURCE_ID,
+            "UTM_SOURCE":request['UTM_SOURCE'],
+            "UTM_MEDIUM":request['UTM_MEDIUM'],
+            "UTM_CAMPAIGN":request['UTM_CAMPAIGN'],
+            "UTM_TERM":request['UTM_TERM'],
+            "UTM_CONTENT":request['UTM_CONTENT'],
+            "PHONE": [{ "VALUE": request['PHONE'][0]["VALUE"],"VALUE_TYPE": "OTHER","TYPE_ID": "PHONE"}],
+            'COMMENTS': request['COMMENTS']
+        
+        }}
+    response = requests.post(str(f'{URLBITRIX}/crm.lead.add.json'), json=lead_data)
+    print(response)
+    answ = json.loads(response.text)
+    return {"data": answ['result']}
+
+
+
 @app.post("/addlead/crm.lead.add.json")
 def read_post_root(item: Item):
     lead_data = {'fields':{
@@ -52,6 +94,7 @@ def read_get_root(item: Item):
     print(response)
     answ = json.loads(response.text)
     return {"data": answ['result']}
+
 
 
 
